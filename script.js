b5b9800c5d0af35ff2465331bd5b3a342f3c2d5b
Madina <madinanavrazova@gmail.com>
2025-02-17 19:58:33 +0300

    2 коммит

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ const updateDisplay = () => {
   display.value = currentInput || firstOperand || "0";
 };
 
+// Функция для обработки нажатия кнопки числа
 const handleNumberClick = (number) => {
+  // Обработка случая, если пользователь вводит точку
+  if (number === "." && currentInput.includes(".")) return;
   currentInput += number;
   updateDisplay();
 };
+
+// Функция для обработки нажатия кнопки оператора
 
 const handleOperatorClick = (op) => {
   if (currentInput === "") return;
@@ -18,13 +23,20 @@ const handleOperatorClick = (op) => {
   if (firstOperand === null) {
     firstOperand = parseFloat(currentInput);
   } else if (operator) {
-    firstOperand = calculate(firstOperand, parseFloat(currentInput), operator);
+    const result = calculate(firstOperand, parseFloat(currentInput), operator);
+    if (result === "Error") {
+      alert("На ноль делить нельзя!");
+      resetCalculator();
+      return;
+    }
+    firstOperand = result;
   }
   operator = op;
   currentInput = "";
   updateDisplay();
 };
 
+// Функция для выполнения вычислений
 const calculate = (a, b, op) => {
   switch (op) {
     case "+":
@@ -35,17 +47,27 @@ const calculate = (a, b, op) => {
       return a * b;
     case "/":
       return b === 0 ? "Error" : a / b;
+    default:
+      return 0;
   }
 };
 
+// Функция для обработки нажатия кнопки равенства
 const handleEqualClick = () => {
   if (firstOperand === null || currentInput === "" || operator === "") return;
-  firstOperand = calculate(firstOperand, parseFloat(currentInput), operator);
+  const result = calculate(firstOperand, parseFloat(currentInput), operator);
+  if (result === "Error") {
+    alert("На ноль делить нельзя, иди учить математику!!!!");
+    resetCalculator();
+    return;
+  }
+  firstOperand = result;
   currentInput = "";
   operator = "";
   updateDisplay();
 };
 
+// Функция для сброса калькулятора
 const resetCalculator = () => {
   currentInput = "";
   operator = "";
@@ -53,7 +75,18 @@ const resetCalculator = () => {
   updateDisplay();
 };
 
-// Обработчики событий
+const handleNegativeClick = () => {
+  if (currentInput === "") return;
+
+  if (currentInput.startsWith("-")) {
+    currentInput = currentInput.slice(1);
+  } else {
+    currentInput = "-" + currentInput;
+  }
+  updateDisplay();
+};
+
+// Обработчики событий для кнопок калькулятора
 document.querySelectorAll(".btn_calculator").forEach((button) => {
   button.addEventListener("click", () => handleNumberClick(button.textContent));
 });
@@ -68,3 +101,6 @@ document
   .querySelector(".btn_equal")
   .addEventListener("click", handleEqualClick);
 document.querySelector(".btn_clear").addEventListener("click", resetCalculator);
+document
+  .querySelector(".btn_negative")
+  .addEventListener("click", handleNegativeClick);
